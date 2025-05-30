04ed997a65d20add97b7bbd465654081e64e3f7d
Ksenniya <lukkonina1@gmail.com>
2025-05-30 13:23:19 -0300

    improvements alpha

--- common/workflow/converter_v1/workflow_converter_service.py
+++ common/workflow/converter_v1/workflow_converter_service.py
@@ -15,7 +15,7 @@ API_V_WORKFLOWS_ = "api/v1/workflows"
 class CyodaWorkflowConverterService:
     def convert_workflow(self, workflow_contents, entity_name, entity_version, technical_id):
         workflow_contents = enrich_workflow(workflow_contents)
-        workflow_contents['name'] = f"{workflow_contents['name']}:ENTITY_MODEL_VAR:ENTITY_VERSION_VAR:CHAT_ID_VAR"
+        workflow_contents['name'] = f"{entity_name}:{entity_version}:{technical_id}"
         workflow_contents = json.dumps(workflow_contents)
         workflow_contents = workflow_contents.replace("ENTITY_VERSION_VAR", entity_version)
         workflow_contents = workflow_contents.replace("ENTITY_MODEL_VAR", entity_name)
