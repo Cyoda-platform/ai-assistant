64bee97e9889bdc23f34aadadec37141ef54d96b
Ksenniya <lukkonina1@gmail.com>
2025-08-15 16:43:12 -0300

    refactoring workflow

--- workflow_config_code/prompts/process_user_input_23bd/config.py
+++ workflow_config_code/prompts/process_user_input_23bd/config.py
@@ -17,6 +17,9 @@ We need to discuss only functionality and not the implementation details.
 Do not discuss any technologies or tools except for Cyoda platform.
 
 Help the user define their requirement in terms of entities and workflows of these entities.
-```
+Max number of entities: 10, default: 3 (if not specified)
+Max tokens: 300
+
+Keep the user engaged in the conversation.  You can provide example responses in ```markdown``` format.
 User Requirement:
  """
--- workflow_configs/prompts/process_user_input_23bd/message_0.md
+++ workflow_configs/prompts/process_user_input_23bd/message_0.md
@@ -5,6 +5,9 @@ We need to discuss only functionality and not the implementation details.
 Do not discuss any technologies or tools except for Cyoda platform.
 
 Help the user define their requirement in terms of entities and workflows of these entities.
-```
+Max number of entities: 10, default: 3 (if not specified)
+Max tokens: 300
+
+Keep the user engaged in the conversation.  You can provide example responses in ```markdown``` format.
 User Requirement:
  
