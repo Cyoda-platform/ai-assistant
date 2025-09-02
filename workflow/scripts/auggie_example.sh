6dd058d261de4aea668c1320474d7261b3f18f01
Ksenniya <lukkonina1@gmail.com>
2025-09-02 15:36:02 -0300

    added augment code

--- workflow/scripts/auggie_example.sh
+++ workflow/scripts/auggie_example.sh
@@ -61,21 +61,8 @@ setup_environment() {
     export FORCE_COLOR=0
     export NODE_ENV=production
 
-    # Set Java and build tool environment (auto-detect Java version)
-    if [ -d "/usr/lib/jvm/temurin-17-jdk-amd64" ]; then
-        export JAVA_HOME=/usr/lib/jvm/temurin-17-jdk-amd64
-        log "Using Temurin Java 17"
-    elif [ -d "/usr/lib/jvm/java-21-openjdk-amd64" ]; then
-        export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
-        log "Using OpenJDK Java 21"
-    elif [ -d "/usr/lib/jvm/java-17-openjdk-amd64" ]; then
-        export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
-        log "Using OpenJDK Java 17"
-    else
-        export JAVA_HOME=$(readlink -f /usr/bin/java | sed 's:/bin/java::')
-        log "Using Java from: $JAVA_HOME"
-    fi
-
+    # Set Java 21 and build tool environment
+    export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
     export PATH=$PATH:$JAVA_HOME/bin:/opt/gradle/gradle-8.10.2/bin
 
     # Ensure Gradle wrapper is executable if it exists
