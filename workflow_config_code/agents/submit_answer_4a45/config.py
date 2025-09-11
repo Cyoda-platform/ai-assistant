b11a433eb7682a7767045f72dd6ae567e936da96
Ksenniya <lukkonina1@gmail.com>
2025-09-10 23:07:26 -0300

    added python app workflow

--- workflow_config_code/agents/submit_answer_4a45/config.py
+++ workflow_config_code/agents/submit_answer_4a45/config.py
@@ -7,7 +7,7 @@ Configuration data for the agent.
 
 from typing import Any, Dict, Callable
 
-from workflow_config_code.tools.edit_general_application_java.tool import EditGeneralApplicationToolConfig
+from workflow_config_code.tools.edit_general_application.tool import EditGeneralApplicationToolConfig
 from workflow_config_code.tools.get_user_info_d8dc.tool import GetUserInfoD8dcToolConfig
 from workflow_config_code.tools.web_search_53be.tool import WebSearch53beToolConfig
 from workflow_config_code.tools.read_link_3fd1.tool import ReadLink3fd1ToolConfig
